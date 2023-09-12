6f733cce12189d29bda31ddb67fa08a91289040b
Cr721432 <danyfalcon3@gmail.com>
2023-09-11 21:02:13 -0600

    Codificacion de Producto Unitarias

--- VideoDa.PruebasUnitarias/ProductoDALTests.cs
+++ VideoDa.PruebasUnitarias/ProductoDALTests.cs
@@ -5,46 +5,84 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using VideoDa.EntidadesDeNegocio;
 
 namespace VideoDa.AccesoADatos.Tests
 {
     [TestClass()]
     public class ProductoDALTests
     {
+        private static Producto productoInicial = new Producto { Id = 2, IdCategoria = 2, IdConsola = 2 };
         [TestMethod()]
-        public void CrearAsyncTest()
+        public async Task T1CrearAsyncTest()
         {
-            Assert.Fail();
+            var producto = new Producto();
+            producto.IdCategoria = productoInicial.IdCategoria;
+            producto.IdConsola = productoInicial.IdConsola;
+            producto.Nombre = "The Legend of Zelda: Breath of the Wild";
+            producto.Descripcion = "Explora el mundo de Hyrule en este épico juego de aventuras.";
+            producto.Cantidad = "30";
+            producto.Precio = "$49.99";
+            producto.Imagen = "Zelda Breath of the Wild.pnj";
+            int result = await ProductoDAL.CrearAsync(producto);
+            Assert.AreNotEqual(0, result);
+            productoInicial.Id = producto.Id;
         }
 
         [TestMethod()]
-        public void ModificarAsyncTest()
+        public async Task T2ModificarAsyncTest()
         {
-            Assert.Fail();
+            var producto = new Producto();
+            producto.IdCategoria = productoInicial.IdCategoria;
+            producto.IdConsola = productoInicial.IdConsola;
+            producto.Nombre = "The Legend of Zelda: Breath of the Wild";
+            producto.Descripcion = "Explora el mundo de Hyrule en este épico juego de aventuras.";
+            producto.Cantidad = "30";
+            producto.Precio = "$49.99";
+            producto.Imagen = "Zelda Breath of the Wild.pnj";
+            int result = await ProductoDAL.ModificarAsync(producto);
+            Assert.AreNotEqual(0, result);
         }
 
         [TestMethod()]
-        public void EliminarAsyncTest()
+        public async Task T3EliminarAsyncTest()
         {
-            Assert.Fail();
+            var producto = new Producto();
+            producto.Id = productoInicial.Id;
+            int result = await ProductoDAL.EliminarAsync(producto);
+            Assert.AreNotEqual(0, result);
         }
 
         [TestMethod()]
-        public void ObtenerPorIdAsyncTest()
+        public async Task T4ObtenerPorIdAsyncTest()
         {
-            Assert.Fail();
+            var producto = new Producto();
+            producto.Id = productoInicial.Id;
+            var resultProducto = await ProductoDAL.ObtenerPorIdAsync(producto);
+            Assert.AreEqual(producto.Id, resultProducto.Id);
         }
 
         [TestMethod()]
-        public void ObtenerTodosAsyncTest()
+        public async Task T5ObtenerTodosAsyncTest()
         {
-            Assert.Fail();
+            var resultProducto = await ProductoDAL.ObtenerTodosAsync();
+            Assert.AreNotEqual(0, resultProducto.Count);
         }
 
         [TestMethod()]
-        public void BuscarAsyncTest()
+        public async Task T6BuscarAsyncTestAsync()
         {
-            Assert.Fail();
+
+            var producto = new Producto();
+            producto.IdCategoria = productoInicial.IdCategoria;
+            producto.IdConsola = productoInicial.IdConsola;
+            producto.Nombre = "A";
+            producto.Descripcion = "a";
+            producto.Cantidad = "a";
+            producto.Precio = "a";
+            producto.Imagen = "a";
+            var resultProducto = await ProductoDAL.BuscarAsync(producto);
+            Assert.AreNotEqual(0, resultProducto.Count);
         }
     }
 }
