e02a03904bfe23521f1be29f5cbc82b0fc4e476c
unknown <montanoisrael77@gmail.com>
2023-09-11 19:18:09 -0600

    Codificacion de Articulo Unitarias

--- VideoDa.PruebasUnitarias/ArticuloDALTests.cs
+++ VideoDa.PruebasUnitarias/ArticuloDALTests.cs
@@ -5,46 +5,75 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using VideoDa.EntidadesDeNegocio;
 namespace VideoDa.AccesoADatos.Tests
 {
     [TestClass()]
     public class ArticuloDALTests
     {
+        private static Articulo articuloInicial = new Articulo { Id = 2, IdConsola = 2 };
         [TestMethod()]
-        public void CrearAsyncTest()
+        public async Task T1CrearAsyncTest()
         {
-            Assert.Fail();
+            var articulo = new Articulo();
+            articulo.IdConsola = articuloInicial.IdConsola;
+            articulo.Nombre = "Jack and Daxter III";
+            articulo.Imagen = "JackIII.png";
+            articulo.Descripcion = "Es un juego de accion  y aventura donde exploraras al limite junto a tu mejor amigo Daxter";
+            int result = await ArticuloDAL.CrearAsync(articulo);
+            Assert.AreNotEqual(0, result);
+            articuloInicial.Id = articulo.Id;
+        }
+        [TestMethod()]
+        public async Task T2ModificarAsyncTest()
+        {
+            var articulo = new Articulo();
+            articulo.Id = articuloInicial.Id;
+            articulo.IdConsola = articuloInicial.IdConsola;
+            articulo.Nombre = "Jack and Daxter III";
+            articulo.Imagen = "JackIII.png";
+            articulo.Descripcion = "Es un juego de accion  y aventura donde exploraras al limite junto a tu mejor amigo Daxter";
+            int result = await ArticuloDAL.ModificarAsync(articulo);
+            Assert.AreNotEqual(0, result);
         }
 
         [TestMethod()]
-        public void ModificarAsyncTest()
+        public async Task T3EliminarAsyncTest()
         {
-            Assert.Fail();
+            var articulo = new Articulo();
+            articulo.Id = articuloInicial.Id;
+            int result = await ArticuloDAL.EliminarAsync(articulo);
+            Assert.AreNotEqual(0, result);
         }
 
         [TestMethod()]
-        public void EliminarAsyncTest()
+        public async Task T4ObtenerPorIdAsyncTest()
         {
-            Assert.Fail();
+            var articulo = new Articulo();
+            articulo.Id = articuloInicial.Id;
+            var resultArticulo = await ArticuloDAL.ObtenerPorIdAsync(articulo);
+            Assert.AreEqual(articulo.Id, resultArticulo.Id);
         }
 
         [TestMethod()]
-        public void ObtenerPorIdAsyncTest()
+        public async Task T5ObtenerTodosAsyncTest()
         {
-            Assert.Fail();
+            var resultArticulo = await ArticuloDAL.ObtenerTodosAsync();
+            Assert.AreNotEqual(0, resultArticulo.Count);
         }
 
         [TestMethod()]
-        public void ObtenerTodosAsyncTest()
+        public async Task T6BuscarAsyncTest()
         {
-            Assert.Fail();
-        }
+            var articulo = new Articulo();
+            articulo.IdConsola = articuloInicial.IdConsola;
+            articulo.Nombre = "A";
+            articulo.Imagen = "a";
+            articulo.Descripcion = "a";
+            articulo.Top_Aux = 10;
+            var resultarticulo = await ArticuloDAL.BuscarAsync(articulo);
+            Assert.AreNotEqual(0, resultarticulo.Count);
 
-        [TestMethod()]
-        public void BuscarAsyncTest()
-        {
-            Assert.Fail();
         }
     }
 }
